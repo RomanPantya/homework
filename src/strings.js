6aa23e6ee17d19397ec4fb605417ea811acea70e
nik <n.moiseienko@selectoglobal.com>
2022-05-17 15:06:09 +0300

    strings.js

---
+++ src/strings.js
@@ -0,0 +1,12 @@
+// we have string (lines between tags (<>)):
+//  <
+//                  Bob say: "How are you?"
+//                  Tom asnwered: "I'm fine!"
+//                  Bob: "Open https://www.google.com"
+//                  Tom: "Is it Microsoft's site?"
+//                  Bob: "No"
+//  >
+// your task:
+// 1. create three variables <singleQuotedStr, doubleQuotedStr, templateStr>
+// and assign our string to each of these variables using <'', "", ``> quotes respectivly to string's names
+// 2. compare all strings between each other (google in help) to be sure that they absolutely the same
