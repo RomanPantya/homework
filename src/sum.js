e175159c09b890624db79590db2b1fa6442ec717
roma <romanpantya@gmail.com>
2022-05-17 11:09:24 +0300

    hat

--- src/sum.js
+++ src/sum.js
@@ -2,3 +2,9 @@
 // create variable <b> with value <7>
 // create variable <sum> and store the sum of <a> and <b>
 // log in terminal string "11 + 7 = 18" using all variables and template strings
+
+const a = 11;
+const b = 7;
+const sum = a + b;
+
+console.log()
