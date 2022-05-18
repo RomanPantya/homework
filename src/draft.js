9786d5618317da617eac57e767c773989241609d
roma <romanpantya@gmail.com>
2022-05-18 16:56:58 +0300

    1

---
+++ src/draft.js
@@ -0,0 +1,7 @@
+function myLocalScope() {
+    // Змініть код лише під цим рядком
+  const myVar = 'war';
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
--- src/strings.js
+++ src/strings.js
@@ -10,3 +10,8 @@
 // 1. create three variables <singleQuotedStr, doubleQuotedStr, templateStr>
 // and assign our string to each of these variables using <'', "", ``> quotes respectivly to string's names
 // 2. compare all strings between each other (google in help) to be sure that they absolutely the same
+const singleQuotedStr = '\tBob say: "How are you?"\n\tTom asnwered: "I\'m fine!"\n\tBob: "Open https://www.google.com"\n\tTom: "Is it Microsoft\'s site?"\n\tBob: "No"';
+console.log(singleQuotedStr);
+
+
+
