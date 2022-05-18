656389406e1fab906a9eba02ba463e909e4c834b
roma <romanpantya@gmail.com>
2022-05-18 17:36:13 +0300

    2

--- src/strings.js
+++ src/strings.js
@@ -10,8 +10,23 @@
 // 1. create three variables <singleQuotedStr, doubleQuotedStr, templateStr>
 // and assign our string to each of these variables using <'', "", ``> quotes respectivly to string's names
 // 2. compare all strings between each other (google in help) to be sure that they absolutely the same
+
 const singleQuotedStr = '\tBob say: "How are you?"\n\tTom asnwered: "I\'m fine!"\n\tBob: "Open https://www.google.com"\n\tTom: "Is it Microsoft\'s site?"\n\tBob: "No"';
-console.log(singleQuotedStr);
+//console.log(singleQuotedStr);
+
+const doubleQuotedStr = "\tBob say: \"How are you?\"\n\tTom asnwered: \"I'm fine!\"\n\tBob: \"Open https://www.google.com\"\n\tTom: \"Is it Microsoft's site?\"\n\tBob: \"No\"";
+//console.log(doubleQuotedStr);
+
+const templateStr = `\tBob say: "How are you?"
+\tTom asnwered: "I'm fine!"
+\tBob: "Open https://www.google.com"
+\tTom: "Is it Microsoft's site?"
+\tBob: "No"`;
+//console.log(templateStr);
+
+console.log(singleQuotedStr === doubleQuotedStr && singleQuotedStr === templateStr && doubleQuotedStr === templateStr);
+
+
 
 
 
