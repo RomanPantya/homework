7ef9225d16a294d946c6119f493bf9c28de44da8
nik <n.moiseienko@selectoglobal.com>
2022-05-23 11:02:17 +0300

    objects.js

---
+++ src/may23/objects.js
@@ -0,0 +1,9 @@
+// For variable <obj1> define such pairs using dot syntax:
+// <hello: 'world'>, <ok: 'google'>, <best>: 777
+//
+// For variable <obj2> define the same pairs using array syntax
+
+const obj1 = {};
+const obj2 = {};
+
+// YOUR CODE:
