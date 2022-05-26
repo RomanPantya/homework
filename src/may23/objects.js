93f359ad1652c120e8dcf7ca71a858d7b143f89a
roma <romanpantya@gmail.com>
2022-05-26 17:59:30 +0300

    objects is full

--- src/may23/objects.js
+++ src/may23/objects.js
@@ -3,10 +3,11 @@
 //
 // For variable <obj2> define the same pairs using array syntax
 
-let obj1 = {};
+const obj1 = {};
 const obj2 = {};
 
 // YOUR CODE:
+
 obj1.hello = 'world', obj1.ok = 'google', obj1.best = 777;
 
 obj2['hello'] = 'world', obj2['ok'] = 'google', obj2['best'] = 777;
