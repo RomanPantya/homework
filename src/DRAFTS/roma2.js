b239ce2b1a66914325eb8585e1a0ef9efe2e4d6f
roma <romanpantya@gmail.com>
2022-05-26 17:39:27 +0300

    objects is full

--- src/DRAFTS/roma2.js
+++ src/DRAFTS/roma2.js
@@ -1,2 +1,9 @@
-var arr = ['a', 'b', 'c'];
-console.log(Object.keys(arr));
+const obj1 = {};
+const obj2 = {};
+
+// YOUR CODE:
+obj1.hello = 'world', obj1.ok = 'google', obj1.best = 777;
+obj2['hello'] = 'world', obj2['ok'] = 'google', obj2['best'] = 777;
+
+console.log(obj1);
+console.log(obj2);
--- src/may23/objects.js
+++ src/may23/objects.js
@@ -3,7 +3,10 @@
 //
 // For variable <obj2> define the same pairs using array syntax
 
-const obj1 = {};
+let obj1 = {};
 const obj2 = {};
 
 // YOUR CODE:
+obj1.hello = 'world', obj1.ok = 'google', obj1.best = 777;
+
+obj2['hello'] = 'world', obj2['ok'] = 'google', obj2['best'] = 777;
