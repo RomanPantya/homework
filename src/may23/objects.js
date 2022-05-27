5b992ea491efb61a95a4999e2eaf7cb1be4c4afc
roma <romanpantya@gmail.com>
2022-05-27 14:45:56 +0300

    objects

--- src/may23/objects.js
+++ src/may23/objects.js
@@ -8,6 +8,10 @@ const obj2 = {};
 
 // YOUR CODE:
 
-obj1.hello = 'world', obj1.ok = 'google', obj1.best = 777;
+obj1.hello = 'world'; 
+obj1.ok = 'google';
+obj1.best = 777;
 
-obj2['hello'] = 'world', obj2['ok'] = 'google', obj2['best'] = 777;
+obj2['hello'] = 'world'; 
+obj2['ok'] = 'google'; 
+obj2['best'] = 777;
